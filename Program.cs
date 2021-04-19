0e0f7e42ae14d0181c8ee8fe5fef13848fa29469
LatixXGod <79203041+LatixXGod@users.noreply.github.com>
2021-04-19 20:07:45 +0300

    HomeWork 1 - 2

--- Program.cs
+++ Program.cs
@@ -1,121 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace HomeWork_2__1_1
+namespace Homework_2_1_2
 {
     class Program
     {
-        public class TestCase
+        public static int StrangeSum(int[] inputArray)
         {
-
-            public int d { get; set; }
-
-            public int n { get; set; }
-
-            public int Expected { get; set; }
-            public Exception ExpectedException { get; set; }
-        }
-        static void Testfunc(TestCase testCase)
-        {
-            try
+            int sum = 0;
+            for (int i = 0; i < inputArray.Length; i++) //0(N)
             {
-                var actual = Block(testCase.d);
+                for (int j = 0; j < inputArray.Length; j++)// 0(N^2)
+                {
+                    for (int k = 0; k < inputArray.Length; k++)// 0(N^3)
+                    {
+                        int y = 0;
 
-                if (actual == testCase.Expected)
-                {
-                    Console.WriteLine("Валид");
-                }
-                else
-                {
-                    Console.WriteLine("Не валид");
+                        if (j != 0)
+                        {
+                            y = k / j;
+                        }
+
+                        sum += inputArray[i] + i + k + j + y;
+                    }
                 }
             }
-            catch (Exception x)
-            {
-                if (testCase.ExpectedException != null)
-                {                   
-                    Console.WriteLine("Валид");
-                }
-                else
-                {
-                    Console.WriteLine("Не валид");
-                }
-            }
+
+            return sum;
         }
-        static void Main(string[] args)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("Введите n: ");
-            var testCase1 = new TestCase()
-            {
-                n = 7,
-                Expected = 0,
-                ExpectedException = null
-            };
-
-            var testCase2 = new TestCase()
-            {
-                n = 5,
-                Expected = 0,
-                ExpectedException = null
-            };
-
-            Testfunc(testCase1);
-            Testfunc(testCase2);
-
-        }
-        static int Block(int n)
-        {
-            
-            
-              
-                int d = 0;
-                int i = 2;
-            // n = Convert.ToInt32(Console.ReadLine());
-            while (i <= n)
-                {
-                    if (i < n)
-                    {
-                        if (n % i == 0)
-                        {
-                            d++;
-                            i++;
-                        }
-                        else
-                        {
-                            i++;
-                        }
-                    }
-
-                    else
-                    {
-                        if (d == 0)
-                        {
-                            Console.WriteLine("Простое ");
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Не простое");
-                            break;
-
-                        }
-
-                    }
-            
-                }
-                          return n;
-
-
-
-        }        
-
-
-
-
-    }      
-
+    }
 }
-    
-
