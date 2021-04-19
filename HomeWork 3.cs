801340d8c23d15a0c0ed49620c21a73a57592508
LatixXGod <79203041+LatixXGod@users.noreply.github.com>
2021-04-19 20:15:29 +0300

    Add files via upload

---
+++ HomeWork 3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Homework1_3
+{
+    class Program
+    {
+        static int FibonachiFor(int n)
+        {
+            int f0 = 0;
+            int f1 = 1;
+            int sum = 0;
+            for (int i = 1; i < n; i++)
+            {
+                sum = f0 + f1;
+                f0 = f1;
+                f1 = sum;
+            }
+            return n switch
+            {
+                0 => 0,
+                1 => 1,
+                _ => sum
+            };
+            static void Main(string[] args)
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    Console.WriteLine(FibonachiFor(i));
+                }
+                Console.WriteLine();
+                for (int i = 0; i < 20; i++)
+                {
+                    Console.WriteLine(FibonachiFor(i));
+                }
+            }
+        }
+        static int FibonachiRecurs(int n) 
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return FibonachiRecurs(n - 1) + FibonachiRecurs(n - 2);
+        }
+        
+       
+    }
+}
